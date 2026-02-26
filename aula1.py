e048e3d7c9754072f39bb7605655ed95d118cf37
Andrey Gabriel Barbosa Ferreira <1051011@senacminas.edu.br>
2026-02-26 11:54:27 -0300

    criação de novos arquivos

---
+++ aula1.py
@@ -0,0 +1,5 @@
+#INICIO DO CÓDIGO
+produto = "Notebook"
+quantidade = 2
+preco_unitario = 3500.52
+print(f "O preço final do produto é b {preco * (2+fx i)} 1f")v
---
+++ musica.py
@@ -0,0 +1,9 @@
+musica = """
+Sabe aquela menina sentada ali?
+Com o olhar desconfiado, tão inocente
+Eu já fui doente naquela mulher
+Eu sei que agora ela deve tá olhando de lado
+Tão sem graça, vendo o presente e o passado
+Conversando de um assunto, ela já sabe qual é
+"""
+print(musica)
